503f58a2480856d89487b9ea1f6ae7992ab94960
Seoji <seojiwoong0870@gmail.com>
2024-08-06 23:58:07 +0900

    solution

---
+++ python/IntersectionOfTwoLinkedLists2.py
@@ -0,0 +1,24 @@
+# Runtime: 75 ms
+# Memory Usage: 27.2 MB
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        # if not headA or not headB:
+        #     return None
+        a ,b = headA, headB
+        
+        while a != b:
+            # ea , eb = a.val , b.val
+            # if ea == eb:
+                
+                
+            a = a.next if a else headB
+            b = b.next if b else headA
+        return a
+            
