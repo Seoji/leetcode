42b56e606dc7edb988a90bb8a6c19c4808e21d10
Seoji <seojiwoong0870@gmail.com>
2024-08-14 00:12:43 +0900

    solution

--- python/MaximumNumberOfVowelsInASubstringOfGivenLength.py
+++ python/MaximumNumberOfVowelsInASubstringOfGivenLength.py
@@ -1,18 +1,28 @@
+# Runtime: 141ms
+# Memory: 18.04MB
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        result = 0
-        substring = None
-        for cursor in range(k-1, len(s)):
-            if not substring:
-                substring = s[0:cursor+1]
-            else:
-                substring = substring[1:] + s[cursor]
-            substr_cnt = len(list((filter(lambda ch: ch in ["a", "e", "i", "o", "u"], substring))))
-            result = max(
-                result,
-                substr_cnt
-            )
+        vowel_list = ["a", "e", "i", "o", "u"]        
+        sub_string = s[0:k]
+        result = len(list((filter(lambda ch: ch in vowel_list, sub_string))))
+        current_cnt = result
 
+        for cursor in range(k, len(s)):
+            remove_ch = s[cursor-k]
+            add_ch = s[cursor]
+
+            if add_ch in vowel_list:
+                current_cnt += 1
+            
+            if remove_ch in vowel_list:
+                current_cnt -= 1
+            
+            result = max(result, current_cnt)
             if result == k:
                 break
+
         return result
+
+
+
+
